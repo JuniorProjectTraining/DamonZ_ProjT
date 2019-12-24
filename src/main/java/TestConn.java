34654b9d11ed09718c465f29a3df845ecc525577
unknown <mrscarletzhou@gmail.com>
2019-12-24 15:09:36 +0800

    15_09/24/12

---
+++ src/main/java/TestConn.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class TestConn {
+    public static void main(String[] args) {
+        Connection connection = null;
+        String url = "jdbc:mysql://localhost:3306/hellomaven";
+        String username = "root";
+        String password = "zycfs0827";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            long start = System.currentTimeMillis();
+
+            connection = DriverManager.getConnection(url,username,password);
+
+            long end = System.currentTimeMillis();
+            System.out.println(connection);
+            System.out.println("total time spend:"+(end - start)+"ms");
+
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
