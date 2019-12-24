2207bcef98d5e4317f2c340d083eb16bfed3dd88
unknown <mrscarletzhou@gmail.com>
2019-12-24 20:48:48 +0800

    trans_to_local_pc

--- src/main/java/cn/damonzzz/helloServlet.java
+++ src/main/java/cn/damonzzz/helloServlet.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@WebServlet("/helloServlet")
 public class helloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request,response);
+        //doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
